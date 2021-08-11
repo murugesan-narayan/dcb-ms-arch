950af13d23c1627fdf01fb06b35c11e2816a9f79
Murugesan <murugesan.narayan@gmail.com>
2021-08-11 13:02:46 +0200

    replaced dept with generic Map

--- user-service/src/main/java/com/muru/dcb/user/service/UserService.java
+++ user-service/src/main/java/com/muru/dcb/user/service/UserService.java
@@ -2,16 +2,20 @@ package com.muru.dcb.user.service;
 
 import com.muru.dcb.user.model.User;
 import com.muru.dcb.user.respository.UserRepository;
-import com.muru.dcb.user.vo.Department;
 import com.muru.dcb.user.vo.ResponseTemplateVO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.Map;
+
 @Service
 public class UserService {
-    private UserRepository userRepository;
-    private RestTemplate restTemplate;
+    private final UserRepository userRepository;
+    private final RestTemplate restTemplate;
     @Autowired
     public UserService(UserRepository repository, RestTemplate restTemplate){
         this.userRepository = repository;
@@ -24,9 +28,12 @@ public class UserService {
 
     public ResponseTemplateVO getUserWithDepartment(Long userId) {
         User user = userRepository.findByUserId(userId);
-        Department department = restTemplate.getForObject(
-                "http://DEPARTMENT-SERVICE/departments/"+user.getDepartmentId(), Department.class);
-        ResponseTemplateVO vo = new ResponseTemplateVO(department, user);
-        return vo;
+        ResponseEntity<Map<String, Object>> entity = restTemplate.exchange(
+                "http://DEPARTMENT-SERVICE/departments/"+user.getDepartmentId(),
+                HttpMethod.GET, null,
+                new ParameterizedTypeReference<Map<String, Object>>() {
+                });
+        Map<String, Object> department = entity.getBody();
+        return new ResponseTemplateVO(department, user);
     }
 }
--- user-service/src/main/java/com/muru/dcb/user/vo/ResponseTemplateVO.java
+++ user-service/src/main/java/com/muru/dcb/user/vo/ResponseTemplateVO.java
@@ -5,10 +5,12 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.Map;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class ResponseTemplateVO {
-    private Department department;
+    private Map<String, Object> department;
     private User user;
 }
